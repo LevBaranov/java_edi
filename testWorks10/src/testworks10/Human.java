8fe8ce3d727d64bbe588d8f2b59b6881977d0bf7
LevBaranov <lev.l.baranov@gmail.com>
2018-11-17 18:29:06 +0500

    Задание 10

---
+++ testWorks10/src/testworks10/Human.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testworks10;
+import middleearth.*;
+/**
+ *
+ * @author levbaranov
+ */
+public class Human extends MiddleEarthCitizen {
+    private String homeTown;
+    private int age;
+    
+    public Human(String homeTown, int age, int height, String name) {
+        super(name, height);
+        this.homeTown = homeTown;
+        this.age = Integer.valueOf(age);
+    }
+    public String setHomeTown(String t){
+        this.homeTown = t;
+        return this.homeTown;
+    }
+    public String getHomeTown(){
+        return this.homeTown;
+    }
+    public int setAge(int a){
+        this.age = a;
+        return this.age;
+    }
+    public int getAge(){
+        return this.age;
+    }
+}
