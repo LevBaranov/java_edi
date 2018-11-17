55338da98b5858c1453851fe6f4f17d484990962
LevBaranov <lev.l.baranov@gmail.com>
2018-11-18 01:02:12 +0500

    Добавлены Троль и Рохирим

---
+++ BattleForMiddleEarth/src/middleearth/Rohhirim.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package middleearth;
+
+/**
+ *
+ * @author levbaranov
+ */
+public class Rohhirim extends Human{
+    
+    public Rohhirim(int age, String name) {
+        super("Рохан", age, 170, name);
+    }
+}
